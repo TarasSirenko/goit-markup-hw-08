0aa755f682ecf92c7cb3c4a18ca4a8f83763b215
Taras Sirenko <tarassirenko71@gmail.com>
2022-01-13 17:19:01 +0200

    adapts modal and mobile menu

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,17 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    // const expanded = 
+    //   menuBtnRef.getAttribute("aria-expanded") 
+    
+    menuBtnRef.classList.toggle("oupen");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    // mobileMenuRef.classList.toggle("oupen");
+  });
+})();
